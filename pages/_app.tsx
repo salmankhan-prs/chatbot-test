217925081ee7ab82ff9ca7d04141d7a33cc95251
Salman Khan <salmankhanprs@gmail.com>
2024-10-15 06:57:42 +0530

    feat: added theme toggle

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,22 @@
-import { AppProps } from 'next/app';
-import { Provider } from 'react-redux';
-import { store } from '../store';
-import '../styles/globals.css';
+import { AppProps } from "next/app";
+import { Provider } from "react-redux";
+import { store } from "../store";
+import { ThemeProvider } from "@/components/theme-provider";
+import "../styles/globals.css";
+import { ThemeToggle } from "@/components/theme-toggle";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Provider store={store}>
-      <Component {...pageProps} />
+      <ThemeProvider
+        attribute="class"
+        defaultTheme="system"
+        enableSystem
+        disableTransitionOnChange
+      >
+        <ThemeToggle />
+        <Component {...pageProps} />
+      </ThemeProvider>
     </Provider>
   );
 }
